861f9e9711b5a5afed8e82f5fad3fb8866e69f3a
garelickja <138186599+garelickja@users.noreply.github.com>
2024-02-12 15:43:19 -0600

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.elements['new-task-description'].value);
+    form.reset()
+  })
+})
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+
+  li.textContent = todo;
+  li.appendChild(btn);
+
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
